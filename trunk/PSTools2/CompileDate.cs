997aa0239184278e834fdb305c122c7b5e9ce7e8
jeanlouis.persat <jeanlouis.persat@6d232958-7d05-83cd-1d04-186d2947424c>
2013-09-06 09:31:56 +0000

    * Lisfont disabled when saving AI to bitmap

--- trunk/PSTools2/CompileDate.cs
+++ trunk/PSTools2/CompileDate.cs
@@ -1,5 +1,5 @@
 using System;
 public class CompileDate
 {
-	public static DateTime BuildDate = new DateTime(2013, 7, 25, 15, 53, 27);
+	public static DateTime BuildDate = new DateTime(2013, 9, 6, 11, 30, 15);
 }
